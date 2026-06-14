c408924888c121cfdab84e1da2d23b3c4fdda093
ShubhamDasaya <mrshubhsd@gmail.com>
2026-06-14 13:52:09 +0530

    feat: implement core chat interface with real-time messaging, media support, and user profile management

--- src/ui/Avatar.jsx
+++ src/ui/Avatar.jsx
@@ -8,7 +8,7 @@ const charToHue = (char) => {
 
 const getBackendPort = () => {
     if (import.meta.env.VITE_BACKEND_PORT) return import.meta.env.VITE_BACKEND_PORT;
-    if (import.meta.env.PROD) return "https://huskier-willfully-debroah.ngrok-free.dev";
+    if (import.meta.env.PROD) return "http://13.51.33.115:8000";
     return "http://localhost:8000";
 };
 const BackendPORT = getBackendPort();
